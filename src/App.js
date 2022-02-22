705ebb47846e2704696a3a3763f6944f5d15a263
OAltunorgu <ogulcanaltunorgu@gmail.com>
2022-02-23 01:01:19 +0200

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import MemoizedCounter from './components/MemoizedCounter'
+import React, { useState, useEffect, useCallback } from 'react'
 
-export default function App() {
+const App = () => {
+  const [sayi, setSayi] = useState(0);
+  const [theme, setTheme] = useState("dark");
+
+  const getItems = useCallback(() => {
+    return [sayi, sayi + 1, sayi + 2]
+  }, [sayi])
+
+  const backgroundColor = theme === "dark" ? "#000" : "#fff";
+
+  const handleTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  }
+
   return (
-    <div>
-      <MemoizedCounter />
-    </div>
+    <React.Fragment>
+      <input type="number" value={sayi} onChange={e => setSayi(parseInt(e.target.value))} />
+      <button onClick={() => handleTheme()} style={{ background: `${backgroundColor}` }}> Tema Değiştir</button>
+      <ItemsList getItems={getItems} />
+    </React.Fragment>
   )
 }
+
+const ItemsList = ({ getItems }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    setItems(getItems());
+    console.log("İtemleri güncelliyorum.")
+  }, [getItems]);
+
+  return (
+    <div>
+      <ul>
+        {
+          items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+export default App;
