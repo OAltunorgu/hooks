3c89ce6cc05301d5f6585bb2a175bcd3e6a1b23f
OAltunorgu <ogulcanaltunorgu@gmail.com>
2022-02-22 22:54:52 +0200

    useContext - dark theme light theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React, { Component } from 'react'
-import SayHello from './components/SayHello';
+import React, { useState, useContext } from 'react'
 
-class App extends Component {
-  state = {
-    show: true
+export const lightTheme = {
+  type: "light",
+  body: "#E2E2E2",
+  text: "#363537",
+  toggleBorder: "#FFF",
+  gradient: "linear-gradient(#39598A, #79D7ED)",
+};
+
+export const darkTheme = {
+  type: "dark",
+  body: "#363537",
+  text: "#FAFAFA",
+  toggleBorder: "#6B8096",
+  gradient: "linear-gradient(#091236, #1E215D)",
+};
+
+const ThemeContect = React.createContext(null);
+
+function App() {
+
+  const [theme, setTheme] = useState(darkTheme);
+
+  const handleTheme = () => {
+    if (theme.type === "dark") {
+      setTheme(lightTheme);
+    } else {
+      setTheme(darkTheme);
+    }
   }
 
-  handleShow = () => {
-    this.setState(prevState => ({
-      show: !prevState.show
-    }));
-  }
+  return (
+    <div className="App">
+      <button onClick={() => handleTheme()}>{theme.type === "dark" ? "Light Theme" : "Dark Theme"}</button>
 
-  render() {
-    const { show } = this.state;
-    return (
-      <div>
-        <button onClick={this.handleShow}>{show ? "Sakla" : "Göster"}</button>
-        {show && <SayHello />}
-      </div>
-    )
-  }
+      <ThemeContect.Provider value={theme}>
+        <Kare />
+      </ThemeContect.Provider>
+    </div>
+  )
 }
+
+const Kare = () => {
+  const theme = useContext(ThemeContect);
+  return (
+    <div style={{ background: `${theme.gradient}` }}> Temam: {theme.type}</div >
+  )
+}
+
+
 export default App;
