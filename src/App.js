b15889f46cf265175f534f212cc718ef8dc50d5e
OAltunorgu <ogulcanaltunorgu@gmail.com>
2022-02-28 20:14:53 +0200

    custom hook

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
-import React, { useState, useReducer } from 'react'
-import { act } from 'react-dom/test-utils';
-import { ACTIONS } from "./actionTypes";
-import Todo from './components/Todo';
-
-const reducer = (todos, action) => {
-  switch (action.type) {
-    case ACTIONS.TODO_EKLE:
-      return [...todos, yeniTodoEkle(action.payload.name)]
-    case ACTIONS.TODO_TAMAMLA:
-      return todos.map(todo => {
-        if (todo.id === action.id) {
-          return { ...todo, tamam: !todo.tamam }
-        }
-        return { ...todo };
-      });
-
-    case ACTIONS.TODO_SIL:
-      return todos.filter(todo => todo.id !== action.id);
-
-    default:
-      return todos;
-  }
-};
-
-const yeniTodoEkle = name => {
-  return { id: Date.now(), name: name, tamam: false };
-}
+import React from 'react'
+import useLocalStorage from './useLocalStorage';
 
 function App() {
-
-  const [todos, dispatch] = useReducer(reducer, []);
-  const [name, setName] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch({ type: ACTIONS.TODO_EKLE, payload: { name: name } });
-    setName("");
-  };
-
+  const [name, setName] = useLocalStorage("name", () => {
+    return "Oğulcan";
+  });
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
-      </form>
-      {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} dispatch={dispatch} />
-      ))}
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
     </div>
   )
 }
--- src/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const ACTIONS = {
-    TODO_EKLE: "todo_ekle",
-    TODO_SIL: "todo_sil",
-    TODO_TAMAMLA: "todo_tamamla"
-};
